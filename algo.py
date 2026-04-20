814b33cf77531bde898d7c90fca7de51fbe23dbe
AntoninMignotPilon <antonin.mignotpilon@viacesi.fr>
2026-04-20 08:32:27 +0200

    feat : Modification du système du génération de sommets

--- algo.py
+++ algo.py
@@ -4,45 +4,77 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
-def random_graph(vertice_count=None, edge_chance=0.3, taux_fermeture=0.1):
+def random_graph(vertice_count=None, k_voisins=3, taux_fermeture=0.1, taux_surcout=0.08, facteur_surcout=2.0):
     if vertice_count is None:
         vertice_count = 100
     vertice_count = random.randint(10, vertice_count)
 
-    edges = [[0 for _ in range(vertice_count)] for _ in range(vertice_count)]
-    for x in range(vertice_count):
-        for y in range(x+1, vertice_count):
-            if random.random() <= edge_chance:
-                edges[x][y] = 1
-                edges[y][x] = 1
-
+    # Positions aléatoires avec distance minimale entre sommets
+    distance_min = 12
+    positions = {}
     for i in range(vertice_count):
-        degree = sum(edges[i])
-        while degree < 2:
-            target = random.randrange(vertice_count)
-            if target != i and edges[i][target] == 0:
-                edges[i][target] = 1
-                edges[target][i] = 1
-                degree += 1
+        for _ in range(200):
+            x = random.uniform(0, 100)
+            y = random.uniform(0, 100)
+            if all(np.sqrt((x - xp) ** 2 + (y - yp) ** 2) >= distance_min
+                   for (xp, yp) in positions.values()):
+                positions[i] = (x, y)
+                break
+        else:
+            positions[i] = (x, y)
+
+    # Connecter chaque sommet à ses k plus proches voisins
+    edges = [[0 for _ in range(vertice_count)] for _ in range(vertice_count)]
+    for i in range(vertice_count):
+        distances = []
+        for j in range(vertice_count):
+            if i != j:
+                xi, yi = positions[i]
+                xj, yj = positions[j]
+                d = np.sqrt((xi - xj) ** 2 + (yi - yj) ** 2)
+                distances.append((d, j))
+        distances.sort()
+        for _, j in distances[:k_voisins]:
+            edges[i][j] = 1
+            edges[j][i] = 1
+
     G = nx.from_numpy_array(np.array(edges))
+    nx.set_node_attributes(G, positions, 'pos')
 
-    for (i,j) in G.edges():
-        G[i][j][('close')] = False
+    # Coût de chaque arête
+    for (i, j) in G.edges():
+        xi, yi = positions[i]
+        xj, yj = positions[j]
+        G[i][j]['cout'] = np.sqrt((xi - xj) ** 2 + (yi - yj) ** 2)
+        G[i][j]['close'] = False
+        G[i][j]['surcout'] = False
 
-    aretes_clients = [(i,j) for (i,j) in G.edges() if i != 0 and j != 0]
+    # --- Fermeture d'arêtes (sauf celles du dépôt) ---
+    aretes_clients = [(i, j) for (i, j) in G.edges() if i != 0 and j != 0]
     nb_close = int(len(aretes_clients) * taux_fermeture)
 
     if nb_close > 0:
         aretes_a_fermer = random.sample(aretes_clients, nb_close)
         for (i, j) in aretes_a_fermer:
             G[i][j]['close'] = True
+
+    # Arêtes avec surcoût
+    aretes_ouvertes_clients = [(i, j) for (i, j) in G.edges()
+                               if not G[i][j]['close'] and i != 0 and j != 0]
+    nb_surcout = int(len(aretes_ouvertes_clients) * taux_surcout)
+
+    if nb_surcout > 0:
+        aretes_surcout_list = random.sample(aretes_ouvertes_clients, nb_surcout)
+        for (i, j) in aretes_surcout_list:
+            G[i][j]['cout'] *= facteur_surcout
+            G[i][j]['surcout'] = True
+
     return G
 
 
+# Affichage
 
-### Affichage
-
-G = random_graph(25, edge_chance=0.05, taux_fermeture=0.1)
+G = random_graph(25, k_voisins=3, taux_fermeture=0.1, taux_surcout=0.08)
 
 couleurs = []
 for node in G.nodes():
@@ -51,27 +83,26 @@ for node in G.nodes():
     else:
         couleurs.append('white')
 
-aretes_ouvertes = [(i, j) for (i, j) in G.edges() if not G[i][j]['close']]
+aretes_normales = [(i, j) for (i, j) in G.edges()
+                   if not G[i][j]['close'] and not G[i][j]['surcout']]
 aretes_fermees = [(i, j) for (i, j) in G.edges() if G[i][j]['close']]
+aretes_surcout = [(i, j) for (i, j) in G.edges() if G[i][j]['surcout']]
 
 
 plt.figure(figsize=(12, 8))
-pos = nx.spring_layout(G, k=2, iterations=100)
+pos = nx.get_node_attributes(G, 'pos')
 
-nx.draw_networkx_edges(G, pos, edgelist=aretes_ouvertes,
-                       edge_color='black', width=1)
+nx.draw_networkx_edges(G, pos, edgelist=aretes_normales, edge_color='black', width=1)
 
-nx.draw_networkx_edges(G, pos, edgelist=aretes_fermees,
-                       edge_color='red', width=1.5, style='dashed')
+nx.draw_networkx_edges(G, pos, edgelist=aretes_fermees, edge_color='red', width=1.5, style='dashed')
 
-nx.draw_networkx_nodes(G, pos, node_color=couleurs,
-                       node_size=300, edgecolors='black')
+nx.draw_networkx_edges(G, pos, edgelist=aretes_surcout, edge_color='orange', width=2)
+
+nx.draw_networkx_nodes(G, pos, node_color=couleurs, node_size=300, edgecolors='black')
 
 nx.draw_networkx_labels(G, pos, font_size=9)
 
-
+labels_cout = {(i, j): f"{G[i][j]['cout']:.0f}" for (i, j) in G.edges() if not G[i][j]['close']}
+nx.draw_networkx_edge_labels(G, pos, edge_labels=labels_cout, font_size=7)
 
 plt.show()
-
-
-
